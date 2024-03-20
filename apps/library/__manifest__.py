d0d425d4afdea48041b4ea1ba9223514cb7485cf
odooerpdevelopers <odooerpdevelopers@gmail.com>
2024-03-20 18:02:47 +0100

    Add first module library

---
+++ apps/library/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library",
+
+    'summary': "Library Management",
+
+    'description': """
+    Library Management
+    """,
+
+    'author': "Praxya Sol.",
+    'website': "https://www.praxya.com",
+    'category': 'Services',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'application': True, 
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+
+    # always loaded
+    'data': [
+        "security/ir.model.access.csv",
+        "views/library_book.xml",
+        "views/menus.xml",
+    ],      
+}
+
---
+++ apps/library/models/library_book.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+
+    _description = "Library Book Model"
+
+    name = fields.Char(
+        string="Book",
+        size=20,
+    )
+
+    date = fields.Date(string="Date")
+
+    image = fields.Image()
