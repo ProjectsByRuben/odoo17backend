8e58731c26c5036fb708c69616848ed5621035e7
odooerpdevelopers <odooerpdevelopers@gmail.com>
2024-03-29 09:09:15 +0100

    update sale order payments, refactor

--- apps/sale_order_payments/__manifest__.py
+++ apps/sale_order_payments/__manifest__.py
@@ -11,7 +11,7 @@
     'author': "Praxya Sol.",
     'website': "https://www.praxya.com",
     'category': 'Sales',
-    'version': '0.1',
+    'version': '0.2',
     'license': 'AGPL-3',
 
     'depends': ['sale',],
--- apps/sale_order_payments/models/sale_order.py
+++ apps/sale_order_payments/models/sale_order.py
@@ -1,12 +1,48 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+import logging
+from odoo import _, api, models, fields
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _get_domain_payments(self):
-        return [('state', '=', 'done')]
+    transactions_count = fields.Integer(
+        string="Total Done",
+        copy=False,
+        compute="_compute_transactions_count")
+    transaction_done_ids = fields.Many2many(
+        comodel_name='payment.transaction',
+        string="Done Transactions",
+        help="Show Done Transactions",
+        compute='_compute_done_transaction_ids',
+        copy=False,
+        compute_sudo=True
+    )
 
-    transaction_ids = fields.Many2many(domain=_get_domain_payments)
+    @api.depends('transaction_done_ids')
+    def _compute_transactions_count(self):
+        for tr in self:
+            tr.transactions_count = len(tr.transaction_ids)
+
+    @api.depends('transaction_ids')
+    def _compute_done_transaction_ids(self):
+        for trans in self:
+            trans.transaction_done_ids = trans.transaction_ids.filtered(
+                lambda t: t.state == 'done')
+
+    def action_view_transactions(self):
+        res_action = {
+            'name': _('Payments'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'payment.transaction',
+            'target': 'current',
+        }
+        payments = self.transaction_ids.ids
+        if len(payments) == 1:
+            res_action['res_id'] = payments[0]
+            res_action['view_mode'] = 'form'
+        else:
+            res_action['view_mode'] = 'tree,form'
+            res_action['domain'] = [('id', 'in', payments)]
+        return res_action
